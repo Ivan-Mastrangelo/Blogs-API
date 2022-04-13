7006303160cbd69564866fdf293cb5f3f0acfd16
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-13 10:39:10 -0300

    feat: importar todas as bibliotecas e implementar o apontamento da rota user

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use('/users', userRoutes);
+
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
