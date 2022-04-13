bce70301c1659898dc772c5f465a0c07ebd512e6
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-13 17:23:42 -0300

    feat: implementar maneira de receber o token da service e retorná-lo ao cliente

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,10 +3,10 @@ const userService = require('../services/userService');
 const create = async (req, res) => {
   try {
     const { displayName, email, password, image } = req.body;
-
-    const newUser = await userService.create({ displayName, email, password, image });
+    
+    const token = await userService.create({ displayName, email, password, image });
   
-    return res.status(201).json(newUser);
+    return res.status(201).json({ token });
   } catch (error) {
     return res.status(409).json({ message: error.message });
   }
