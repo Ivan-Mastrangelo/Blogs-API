ee082b801d6348491e60477e245622501813f379
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-13 17:22:29 -0300

    feat: importar função que gera o token e implementar o retorno do token

--- services/userService.js
+++ services/userService.js
@@ -1,9 +1,14 @@
 const { User } = require('../models');
+const tokenGenerate = require('../helpers/tokenGenerate');
 
 const create = async ({ displayName, email, password, image }) => {
-  const newUser = await User.create({ displayName, email, password, image });
-                                         
-  return newUser;
+  await User.create({ displayName, email, password, image });
+  
+  const userToken = tokenGenerate({ displayName, email });
+  
+  console.log(userToken);             
+  
+  return userToken;
 };
 
 module.exports = {
