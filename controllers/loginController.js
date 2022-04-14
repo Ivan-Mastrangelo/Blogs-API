c78ea2e0199c30d436f5a294cb1c41485d89736b
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-14 15:35:15 -0300

    feat: criar o arquivo loginController e implementar a respectiva função

---
+++ controllers/loginController.js
@@ -0,0 +1,17 @@
+const loginService = require('../services/loginService');
+
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    
+    const token = await loginService.login({ email, password });
+  
+    return res.status(200).json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  login,
+};
