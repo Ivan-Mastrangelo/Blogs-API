8827bc973d3e36263a2b9c139ca5d235d99e8fe5
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-14 19:12:36 -0300

    feat: implementar método get na rota user com inserção de middleware para validação do token

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const userInMiddleware = require('../middlewares/userMiddleware');
+const tokenVerify = require('../middlewares/tokenVerifyMiddleware');
 
+router.get('/', tokenVerify, userController.getAllUsers);
 router.post('/', userInMiddleware, userController.create);
 
 module.exports = router;
