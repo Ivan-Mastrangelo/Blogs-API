6b8708a70a91758475eb11911a9bc0c35639a484
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-14 14:25:19 -0300

    fix: corrigir parâmetros do joi

--- middlewares/usersMiddlewares.js
+++ middlewares/usersMiddlewares.js
@@ -1,17 +1,22 @@
 const Joi = require('joi');
+const badRequest = require('../error/badRequest');
 
 const userSchema = Joi.object({
-  displayName: Joi.string().min(8).required,
-  email: Joi.string().email().required,
-  password: Joi.string().length(6).required,
+  displayName: Joi.string().min(8).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().length(6).required(),
 });
 
 const userInMiddleware = (req, res, next) => {
-  const { display, email, password } = req.body;
-  const { error } = userSchema.validate(display, email, password);
-  if (error) return res.status(400).json({ message: error.message });
-
-next();
+  try {
+    const { displayName, email, password } = req.body;
+    const { error } = userSchema.validate({ displayName, email, password });
+    if (error) throw badRequest(error.message);
+  
+  next();
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = userInMiddleware;
