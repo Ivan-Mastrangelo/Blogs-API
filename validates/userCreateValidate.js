b1e0931f9a9dbde0f8a4ce11fae991b90962923d
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-14 14:21:28 -0300

    feat: implementar a função de validação de para evitar duplicidade de clientes no caddastro

--- validates/userCreateValidate.js
+++ validates/userCreateValidate.js
@@ -2,8 +2,8 @@ const { User } = require('../models');
 
 const userCreateValidate = async (email) => {
   const userVerify = await User.findOne({ where: { email } });
-  if (userVerify) return res.status(409).jasom({ massage: 'User already registered' });
-  return true;
+  if (userVerify) return true;
+  return false;
 };
 
 module.exports = userCreateValidate;
