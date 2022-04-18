44d6c596b44754ae88714d25f2e09b02dc5f205a
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-18 17:15:44 -0300

    feat: implementar a função de buscar todas as categorias no BD, atravéz da model

--- services/categoriesService.js
+++ services/categoriesService.js
@@ -8,6 +8,12 @@ const create = async (name) => {
   return newCategory;
 };
 
+const getAllCategories = async () => {
+  const categories = await Category.findAll();
+  return categories;
+};
+
 module.exports = {
   create,
+  getAllCategories,
 };
