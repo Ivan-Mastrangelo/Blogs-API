15c9e4386d58985677f51a445b9d67ac90ce7cd1
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-18 15:04:53 -0300

    feat: implementar o recebimento e resposta de requisição de dados de usuários pelo id

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,7 +21,18 @@ const getAllUsers = async (_req, res, next) => {
   }
 };
 
+const getUserById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await userService.getUserById(id);
+    return res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   create,
   getAllUsers,
+  getUserById,
 };
