fab298d4e07e7fc2df9bfa9cc8475e340ce72934
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-18 17:12:34 -0300

    chore: acrescentar o método getAll à rota categories, com o middleware de validação do token

--- routes/categoriesRoutes.js
+++ routes/categoriesRoutes.js
@@ -5,5 +5,6 @@ const categoriesController = require('../controllers/categoriesController');
 const tokenVerify = require('../middlewares/tokenVerifyMiddleware');
 
 router.post('/', tokenVerify, categoriesController.create);
+router.get('/', tokenVerify, categoriesController.getAllCategories);
 
 module.exports = router;
