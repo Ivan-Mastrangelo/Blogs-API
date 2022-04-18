3f5c033a794df04eff311f3560cad8df396a732e
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-18 15:56:02 -0300

    chore: importar e acrescentar o recebimento da rota categories

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const loginRoutes = require('./routes/loginRoutes');
+const categoriesRoutes = require('./routes/categoriesRoutes');
 const errorsMiddleware = require('./middlewares/errorsMiddleware');
 
 const app = express();
@@ -15,6 +16,7 @@ app.get('/', (request, response) => {
 
 app.use('/user', userRoutes);
 app.use('/login', loginRoutes);
+app.use('/categories', categoriesRoutes);
 
 app.use(errorsMiddleware);
 
