d991bbf3554a36b1c7308edb942c35551796f157
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-18 15:54:41 -0300

    feat: criar o arquivo categoriesControllers e implementar o recebimento da requisição e sua devolutiva com categoria criada e tratativa de erro correstpondente

---
+++ controllers/categoriesController.js
@@ -0,0 +1,17 @@
+const categoriesService = require('../services/categoriesService');
+
+const create = async (req, res, next) => {
+  try {
+    const { name } = req.body;
+    
+    const category = await categoriesService.create(name);
+  
+    return res.status(201).json(category);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  create,
+};
