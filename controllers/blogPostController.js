6ba14611aafaed4235a95cb3c400d0dfbfd357d9
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-19 13:00:55 -0300

    feat: implementar função que recebe a requisição e retorna devolutiva para o cliente na camada controller da rota /post/:id

--- controllers/blogPostController.js
+++ controllers/blogPostController.js
@@ -1,27 +1,38 @@
 const blogPostService = require('../services/blogPostService');
 
 const create = async (req, res, next) => {
-    try {
-      const { title, content, categoryIds } = req.body;
-      const { authorization } = req.headers;
-      const blogPost = await blogPostService.create(title, content, categoryIds, authorization);
-    
-      return res.status(201).json(blogPost);
-    } catch (error) {
-      next(error);
-    }
-  };
+  try {
+    const { title, content, categoryIds } = req.body;
+    const { authorization } = req.headers;
+    const blogPost = await blogPostService.create(title, content, categoryIds, authorization);
+  
+    return res.status(201).json(blogPost);
+  } catch (error) {
+    next(error);
+  }
+};
 
-  const getAllPosts = async (_req, res, next) => {
-    try {
-      const posts = await blogPostService.getAllPosts();
-      return res.status(200).json(posts);
-    } catch (error) {
-      next(error);
-    }
-  };
+const getAllPosts = async (_req, res, next) => {
+  try {
+    const posts = await blogPostService.getAllPosts();
+    return res.status(200).json(posts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getPostById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const post = await blogPostService.getPostById(id);
+    return res.status(200).json(post);
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   create,
   getAllPosts,
+  getPostById,
 };
