40d1af538678f7852507c77c91db30e44a73dc34
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-19 16:26:04 -0300

    feat: implementar a funções de atualização do post e posterior busca, para entragar no formato pedido, na camada service

--- services/blogPostService.js
+++ services/blogPostService.js
@@ -2,8 +2,8 @@ const { BlogPost, Category, User } = require('../models');
 const badRequest = require('../error/badRequest');
 const tokenDecrypt = require('../helpers/tokenDecrypt');
 const getPostValidate = require('../validates/getPostValidade');
-// const BlogPostUpdateValidate = require('../validates/BlogPostUpdateValidate');
-// const unauthorized = require('../error/unauthorized');
+const BlogPostUpdateValidate = require('../validates/BlogPostUpdateValidate');
+const unauthorized = require('../error/unauthorized');
 
 const create = async (title, content, categoryIds, authorization) => {
   await Promise.all(categoryIds.map(async (ids) => {
@@ -49,21 +49,22 @@ const getPostById = async (id) => {
   } 
 };
 
-// const update = async (id, authorization, body) => {
-//   const { title, content, categoryIds } = body;
-//   const UpdateValidate = BlogPostUpdateValidate(id, authorization);
-//   if (UpdateValidate === false) throw unauthorized('Unauthorized user');
-//   if (categoryIds) throw badRequest('Categories cannot be edited');
-//   await BlogPost.update({ title, content }, { where: { id } });
-//   const updatedPost = await BlogPost.findByPk(id, {
-//     include: [{ model: Category, as: 'categories' }],
-//   });
-//   return updatedPost;
-// };
+const update = async (body, id, authorization) => {
+  const { title, content, categoryIds } = body;
+  const updateValidate = await BlogPostUpdateValidate(id, authorization);
+  console.log(updateValidate);
+  if (updateValidate === false) throw unauthorized('Unauthorized user');
+  if (categoryIds) throw badRequest('Categories cannot be edited');
+  await BlogPost.update({ title, content }, { where: { id } });
+  const updatedPost = await BlogPost.findByPk(id, {
+    include: [{ model: Category, as: 'categories', through: { attributes: [] } }],
+  });
+  return updatedPost;
+};
 
 module.exports = {
   create,
   getAllPosts,
   getPostById,
-  // update,
+  update,
 };
