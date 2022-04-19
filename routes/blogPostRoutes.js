cb8c84b6bbd6a20a053a595baf98e2b4017da13d
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-19 11:34:15 -0300

    feat: criar a rota /post

--- routes/blogPostRoutes.js
+++ routes/blogPostRoutes.js
@@ -6,5 +6,6 @@ const blogPostMiddleware = require('../middlewares/blogPostMiddleware');
 const tokenVerify = require('../middlewares/tokenVerifyMiddleware');
 
 router.post('/', tokenVerify, blogPostMiddleware, blogPostController.create);
+router.get('/', tokenVerify, blogPostController.getAllPosts);
 
 module.exports = router;
