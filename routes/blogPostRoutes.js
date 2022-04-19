f4cf440d3d5b6b26e1870bf89778a6f11ecacddd
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-19 16:30:00 -0300

    fix: importar middleware do arquivo correto

--- routes/blogPostRoutes.js
+++ routes/blogPostRoutes.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const blogPostController = require('../controllers/blogPostController');
 const blogPostMiddleware = require('../middlewares/blogPostMiddleware');
 const tokenVerify = require('../middlewares/tokenVerifyMiddleware');
-// const updatePostMiddleware = require('../middlewares/blogPostMiddleware');
+const updatePostMiddleware = require('../middlewares/updatePostMiddleware');
 
 router.post('/', tokenVerify, blogPostMiddleware, blogPostController.create);
 router.get('/', tokenVerify, blogPostController.getAllPosts);
 router.get('/:id', tokenVerify, blogPostController.getPostById);
-// router.put('/:id', tokenVerify, updatePostMiddleware, blogPostController.update);
+router.put('/:id', tokenVerify, updatePostMiddleware, blogPostController.update);
 
 module.exports = router;
