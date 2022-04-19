087dfc5c53191552f30d9201424d02a51703011d
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-19 15:28:09 -0300

    feat: acrescentar o método put na rota blogPost

--- routes/blogPostRoutes.js
+++ routes/blogPostRoutes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const blogPostController = require('../controllers/blogPostController');
 const blogPostMiddleware = require('../middlewares/blogPostMiddleware');
 const tokenVerify = require('../middlewares/tokenVerifyMiddleware');
+// const updatePostMiddleware = require('../middlewares/blogPostMiddleware');
 
 router.post('/', tokenVerify, blogPostMiddleware, blogPostController.create);
 router.get('/', tokenVerify, blogPostController.getAllPosts);
 router.get('/:id', tokenVerify, blogPostController.getPostById);
+// router.put('/:id', tokenVerify, updatePostMiddleware, blogPostController.update);
 
 module.exports = router;
