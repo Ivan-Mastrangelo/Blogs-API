b44c368e86853d9b8993e8bf8a08fc13faa7d627
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-19 11:37:05 -0300

    fix: Alterar o alis da tabela Users de Users para user, conforme solicitado pelo requisito 8

--- models/blogpost.js
+++ models/blogpost.js
@@ -2,14 +2,14 @@ module.exports = (sequelize, DataTypes) => {
   const BlogPost = sequelize.define('BlogPost', {
     title: DataTypes.STRING,
     content: DataTypes.STRING,
-    userId: DataTypes.INTEGER,
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
     published: DataTypes.DATE,
     updated: DataTypes.DATE,
   }, { timestamps: false });
 
   BlogPost.associate = (models) => {
     BlogPost.belongsTo(models.User,
-      { foreignKey: 'userId', as: 'Users' });
+      { foreignKey: 'userId', as: 'user' });
   };
 
   return BlogPost;
