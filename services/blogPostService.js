c395f6feb61abd04564c4972a443edb3573d16aa
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-19 15:25:28 -0300

    feat: iniciar implementação da função que atualiza posts já criados

--- services/blogPostService.js
+++ services/blogPostService.js
@@ -1,6 +1,9 @@
 const { BlogPost, Category, User } = require('../models');
 const badRequest = require('../error/badRequest');
 const tokenDecrypt = require('../helpers/tokenDecrypt');
+const getPostValidate = require('../validates/getPostValidade');
+// const BlogPostUpdateValidate = require('../validates/BlogPostUpdateValidate');
+// const unauthorized = require('../error/unauthorized');
 
 const create = async (title, content, categoryIds, authorization) => {
   await Promise.all(categoryIds.map(async (ids) => {
@@ -23,28 +26,44 @@ const getAllPosts = async () => {
         as: 'user',
         attributes: { exclude: ['password'] },
       },
-      // { model: Category, as: 'categories' },
+      { model: Category, as: 'categories', through: { attributes: [] } },
     ],
   });
   return allPosts;
 };
 
 const getPostById = async (id) => {
-  const post = await BlogPost.findByPk(id, {
-    include: [
-      {
-        model: User,
-        as: 'user',
-        attributes: { exclude: ['password'] },
-      },
-      { model: Category, as: 'categories' },
-    ],
-  });
-  return post;
+  const postValidate = await getPostValidate(id);
+  if (postValidate) {
+    const post = await BlogPost.findByPk(id, {
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: { exclude: ['password'] },
+        },
+        { model: Category, as: 'categories' },
+      ],
+    });
+    return post;
+  } 
 };
+
+// const update = async (id, authorization, body) => {
+//   const { title, content, categoryIds } = body;
+//   const UpdateValidate = BlogPostUpdateValidate(id, authorization);
+//   if (UpdateValidate === false) throw unauthorized('Unauthorized user');
+//   if (categoryIds) throw badRequest('Categories cannot be edited');
+//   await BlogPost.update({ title, content }, { where: { id } });
+//   const updatedPost = await BlogPost.findByPk(id, {
+//     include: [{ model: Category, as: 'categories' }],
+//   });
+//   return updatedPost;
+// };
 
 module.exports = {
   create,
   getAllPosts,
   getPostById,
+  // update,
 };
