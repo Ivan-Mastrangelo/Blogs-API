8a4b4f44e80db503bf6807a25f7bae46e9ba1670
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-19 16:41:51 -0300

    fix: corrigir ordem dos parâmetros da função e tornar a chamada do tokenDecrypt em assíncrona

--- validates/BlogPostUpdateValidate.js
+++ validates/BlogPostUpdateValidate.js
@@ -1,11 +1,12 @@
 const { BlogPost } = require('../models');
 const tokenDecrypt = require('../helpers/tokenDecrypt');
 
-const BlogPostUpdateValidate = async (authorization, id) => {
+const BlogPostUpdateValidate = async (id, authorization) => {
+  console.log(authorization);
   const userId = await tokenDecrypt(authorization);
   const post = await BlogPost.findByPk(id);
   if (userId !== post.userId) return false;
-  return true;
+  if (userId === post.userId) return true;
 };
 
 module.exports = BlogPostUpdateValidate;
