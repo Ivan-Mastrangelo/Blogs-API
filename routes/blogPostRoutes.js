43732ee8ffdf9a66d15cea5d8bf8707c59f5dc49
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-20 00:06:40 -0300

    feat: implementar o método delete na rota /post, com validação do token

--- routes/blogPostRoutes.js
+++ routes/blogPostRoutes.js
@@ -10,5 +10,6 @@ router.post('/', tokenVerify, blogPostMiddleware, blogPostController.create);
 router.get('/', tokenVerify, blogPostController.getAllPosts);
 router.get('/:id', tokenVerify, blogPostController.getPostById);
 router.put('/:id', tokenVerify, updatePostMiddleware, blogPostController.update);
+router.delete('/:id', tokenVerify, blogPostController.destroy);
 
 module.exports = router;
