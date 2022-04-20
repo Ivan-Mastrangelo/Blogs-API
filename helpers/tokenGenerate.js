5a960ab20e311fda1bca6759839867d32a0c4de3
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-04-20 19:35:35 -0300

    refactor: passar a chave secret do arquivo, para o arquivo .env e importá-la como variável de ambiente

--- helpers/tokenGenerate.js
+++ helpers/tokenGenerate.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const tokenGenerate = (payload) => {
-const secretKey = 'só$abeQu&mviu';
+const secretKey = process.env.JWT_SECRET;
 
 const jwtConfig = {
   expiresIn: '5d',
